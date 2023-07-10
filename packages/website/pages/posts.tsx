2f261f1da8553ba6ed7d59595f3f648dfda572db
cassandra <cassandra.kesewa@gmail.com>
2023-07-10 09:26:57 +0000

    triggering a deploy

--- packages/website/pages/posts.tsx
+++ packages/website/pages/posts.tsx
@@ -4,6 +4,7 @@ import Layout from '../components/Blog/Layout';
 import { Wrapper } from '@un/react';
 import { getAllPosts } from '../lib/getPost';
 
+//triggering a commit
 const Posts = ({ articles }: any) => {
   return (
     <Layout>
