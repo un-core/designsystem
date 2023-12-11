352a03111cee57a3009a0ff68d0e05fca77f994d
Robert Gühne <robert@wirewire.de>
2023-12-11 08:10:06 +0100

    fix(tests): fixed current tests and added them to the pipeline setup

--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
 
   roots: ["<rootDir>/src"],
   testMatch: [
-    "**/__tests__/**/*.+(ts|tsx|js)",
+    //"**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)",
   ],
 };
--- packages/react/src/components/Accordion/__tests__/ssr.test.ts
+++ packages/react/src/components/Accordion/__tests__/ssr.test.ts
@@ -2,31 +2,31 @@
  * @jest-environment node
  */
 
-import { renderToString } from 'react-dom/server';
-import { getAccordion } from './utils.tsx.new';
+import { renderToString } from "react-dom/server";
+import { getAccordion } from "./utils";
 
-describe('Server rendering', () => {
-  test('defalut state', () => {
+describe("Server rendering", () => {
+  test("defalut state", () => {
     const htmlString = renderToString(getAccordion());
-    expect(htmlString).toContain('szh-accordion__item--status-exited');
-    expect(htmlString).toContain('item 1');
+    expect(htmlString).toContain("szh-accordion__item--status-exited");
+    expect(htmlString).toContain("item 1");
   });
 
-  test('mountOnEnter', () => {
+  test("mountOnEnter", () => {
     const htmlString = renderToString(
       getAccordion({ props: { mountOnEnter: true } })
     );
-    expect(htmlString).toContain('szh-accordion__item--status-unmounted');
-    expect(htmlString).not.toContain('szh-accordion__item--status-exited');
-    expect(htmlString).not.toContain('item 1');
+    expect(htmlString).toContain("szh-accordion__item--status-unmounted");
+    expect(htmlString).not.toContain("szh-accordion__item--status-exited");
+    expect(htmlString).not.toContain("item 1");
   });
 
-  test('initialEntered', () => {
+  test("initialEntered", () => {
     const htmlString = renderToString(
       getAccordion({ props: { initialEntered: true } })
     );
-    expect(htmlString).toContain('szh-accordion__item--status-entered');
-    expect(htmlString).not.toContain('szh-accordion__item--status-exited');
-    expect(htmlString).toContain('item 1');
+    expect(htmlString).toContain("szh-accordion__item--status-entered");
+    expect(htmlString).not.toContain("szh-accordion__item--status-exited");
+    expect(htmlString).toContain("item 1");
   });
 });
